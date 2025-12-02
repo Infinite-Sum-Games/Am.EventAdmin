ecf988c1a2263b64f710f807021127fd04d707ce
vijaysb0613 <vijaysb2006@gmail.com>
2025-12-03 00:47:17 +0530

    feat:get all organisers integrated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,5 +3,7 @@
 const CURRENT_BASE_URL: string = "/api/v1"; 
 
 export const api = {
-    FETCH_ALL_EVENTS: `${CURRENT_BASE_URL}/events`,
+  FETCH_ALL_EVENTS: `${CURRENT_BASE_URL}/events`,
+  FETCH_ALL_ORGANIZERS: `${CURRENT_BASE_URL}/organizers`,
+  CREATE_ORGANIZER: `${CURRENT_BASE_URL}/organizers`,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api/v1": {
-				target: "http://localhost:9000",
+				target: "http://localhost:8080",
 				changeOrigin: true,
 			},
 		},
