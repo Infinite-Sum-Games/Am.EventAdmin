98dc067147714f829835f1a0b49367630960b502
vijaysb0613 <vijaysb2006@gmail.com>
2025-12-03 15:27:57 +0530

    chore:fix delete org endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,25 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [
-		tanstackRouter({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		react(),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	server: {
-		proxy: {
-			"/api/v1": {
-				target: "http://localhost:8080",
-				changeOrigin: true,
-			},
-		},
-	},
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
-
