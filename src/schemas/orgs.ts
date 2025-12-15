ab4292a98aba167c0cf5ffc8c14c4676f789170c
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2025-12-16 00:17:02 +0530

    feat: rename and update organizer schemas for clarity and consistency

--- src/schemas/orgs.ts
+++ src/schemas/orgs.ts
@@ -1,15 +1,18 @@
 import z from "zod";
 
-export const OrgSchema = z.object({
+export const createOrganizerSchema = z.object({
     name: z.string().min(3, "Organizer name must be at least 3 characters").regex(/^[a-zA-Z\s]+$/, "Name must contain only letters"),
     email: z.email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     org_type: z.enum(["DEPARTMENT", "CLUB"], "Organizer type is either Department or Club"),
     student_head: z.string().min(3, "Student head is required").regex(/^[a-zA-Z\s]+$/, "Student head must contain only letters"),
-    student_co_head: z.string().optional().refine(val => !val || /^[a-zA-Z\s]+$/.test(val), "Student co-head must contain only letters"),
+    student_co_head: z.string().optional().refine(val => !val || /^[a-zA-Z\s]+$/.test(val), "Student co-head must contain only letters").optional(),
     faculty_head: z.string().min(3, "Faculty head is required").regex(/^[a-zA-Z\s.]+$/, "Faculty head must contain only letters or symbols like ."),
 });
 
-export const EditOrgSchema = OrgSchema.extend({
-    id: z.uuid("Invalid organization ID")
-})
+export const updateOrganizerSchema = createOrganizerSchema.omit({ password: true }).extend({
+    id: z.uuid("Organizer ID must be a valid UUID"),
+});
+
+export type CreateOrganizerInput = z.infer<typeof createOrganizerSchema>;
+export type UpdateOrganizerInput = z.infer<typeof updateOrganizerSchema>;
