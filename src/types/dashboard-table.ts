661f0ece8a31cb0b55d79101eedd55951bd31eae
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2025-12-28 08:01:22 +0530

    feat: add revenue_without_gst property to Leaderboard interface

--- src/types/dashboard-table.ts
+++ src/types/dashboard-table.ts
@@ -2,6 +2,7 @@ export interface Leaderboard {
     event_id: string;
     event_name: string;
     revenue: number;
+    revenue_without_gst: number;
     seats_filled: number;
     total_seats: number;
     is_group: boolean;
