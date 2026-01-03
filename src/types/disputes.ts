c9c421fccfa2c6ed8a343f45743d03163c2d271e
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-03 06:51:24 +0530

    ref: change DisputeStatus, Dispute, and GetDisputesResponse types to interfaces

--- src/types/disputes.ts
+++ src/types/disputes.ts
@@ -1,20 +1,20 @@
 export type DisputeStatusEnum = 'OPEN' | 'RESOLVED' | 'REJECTED';
 
-export type DisputeStatus = {
+export interface DisputeStatus {
   dispute_status_enum: DisputeStatusEnum;
   valid: boolean;
-};
+}
 
-export type Dispute = {
+export interface Dispute {
   id: string;
   txn_id: string;
   student_email: string | null;
   description: string | null;
   event_id: string;
   dispute_status: DisputeStatus;
-};
+}
 
-export type GetDisputesResponse = {
+export interface GetDisputesResponse {
   message: string;
   disputes: Dispute[];
-};
+}
